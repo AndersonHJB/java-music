96f28ea206c8bf673f6857ffa42ceae3843c8610
AndersonHJB <bornforthis@bornforthis.cn>
2025-03-23 15:52:49 +0800

    添加.idea配置文件，更新application.yml中的路径，移除路由中的订单组件

--- web/src/router/root.js
+++ web/src/router/root.js
@@ -98,7 +98,7 @@ const constantRouterMap = [
     component: () => import('/@/views/admin/main.vue'),
     children: [
       { path: 'overview', name: 'overview', component: () => import('/@/views/admin/overview.vue') },
-      { path: 'order', name: 'order', component: () => import('/@/views/admin/order.vue') },
+      // { path: 'order', name: 'order', component: () => import('/@/views/admin/order.vue') },
       { path: 'thing', name: 'thing', component: () => import('/@/views/admin/thing.vue') },
       { path: 'comment', name: 'comment', component: () => import('/@/views/admin/comment.vue') },
       { path: 'user', name: 'user', component: () => import('/@/views/admin/user.vue') },
